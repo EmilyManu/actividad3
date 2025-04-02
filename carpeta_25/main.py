6cea6299aa7cde8d3daf84213b1b853348ef43ee
Emily Walteros <emilywalterosnieto@gmail.com>
2025-04-02 08:43:35 -0500

    cambio el archivo original

--- carpeta_25/main.py
+++ carpeta_25/main.py
@@ -1,5 +1,8 @@
 # Estudiante: corrige este código y haz un pull request con la versión corregida.
 
 numeros = [10, 20, 30]
-indice = numeros.index(40)
-print(indice)
+if 40 in numeros:
+    indice = numeros.index(40)
+    print(indice)
+else: 
+    print("el  valor no se encuentra en la lista")
