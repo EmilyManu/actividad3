eefbee2ac5922be020679dece8b782c0cab84e4b
Emily Walteros <emilywalterosnieto@gmail.com>
2025-04-02 08:27:46 -0500

    subo codigo corregido

---
+++ carpeta_25/emily correcion codigo .py
@@ -0,0 +1,8 @@
+# Estudiante: corrige este código y haz un pull request con la versión corregida.
+
+numeros = [10, 20, 30]
+if 40 in numeros:
+    indice = numeros.index(40)
+    print(indice)
+else: 
+    print("el  valor no se encuentra en la lista")
